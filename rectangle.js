89816e8026e71e776eb89b0c0e9d7d9835efd9af
zhangyan <491989897@qq.com>
2020-02-26 09:09:27 +0800

    eslint

--- rectangle.js
+++ rectangle.js
@@ -1,23 +1,18 @@
 $(function() {
-    var $width = $('#width'),
-        $height = $('#height'),
-        $btnCal = $('#calculate'),
-        $perimeter = $('#perimeter'),
-        $area = $('#area');
+  var $width = $('#width'),
+      $height = $('#height'),
+      $btnCal = $('#calculate'),
+      $perimeter = $('#perimeter'),
+      $area = $('#area');
 
-function roundFractional(x, n) {
-    return Math.round(x * Math.pow(10, n)) / Math.pow(10, n);
+  $btnCal.click(function(){
+    var w = Number($width.val()),
+        h = Number($height.val());
 
-}
-            $btnCal.click(function(){
-                  var w =Number($width.val()),
-                  h =Number($height.val());
-            var p=2*roundFractional(w+h,1),
-            a=roundFractional(w*h,2);
-$perimeter.val(p);
-$area.val(a);
+    var p = 2 * (w + h),
+        a = w * h;
 
-
-        });
-
+    $perimeter.val(p);
+    $area.val(a);
+  });
 });
